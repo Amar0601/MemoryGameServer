37a61ded935ad11b2f8b06128038b0c46b055559
Amar Bhapkar <bhapkar.amar@gmail.com>
2020-10-14 16:59:17 +0530

    server files updated

---
+++ MemoryGameServer/Controllers/InitController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MemoryGameServer.Controllers
+{
+    [EnableCors("MyPolicy")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InitController : ControllerBase
+    {
+        private IConfiguration configuration;
+
+        public InitController(IConfiguration config)
+        {
+            configuration = config;
+        }
+
+        [HttpPost]
+        public ActionResult<String> Post([FromBody] Schema value)
+        {
+            try
+            {
+                // Generate new GUID for saving game data
+                Guid gameId = Guid.NewGuid();
+                // Read configuration value of data directory for saving the game data
+                string directoryLocation = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, configuration["GameDataDirectory"]);
+                // check if directory exist if not create it.
+                if (!Directory.Exists(directoryLocation))
+                {
+                    Directory.CreateDirectory(directoryLocation);
+                }
+
+                string fileName = gameId + ".json";
+                string filePath = Path.Combine(directoryLocation, fileName);
+                // create file at filePath formed in earliar step
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    value.roundData = new List<KeyValuePair<string, string>>();
+                    stream.Write(Encoding.ASCII.GetBytes(value.ToString()));
+                }
+
+                // return GUID back to the client
+                return JsonConvert.SerializeObject(new { gameId });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occured while creating datafile: " + ex.Message.ToString());
+                throw;
+            }
+        }
+
+        [HttpPut]
+        public ActionResult<String> Put([FromBody] Round value)
+        {
+
+            // set value based on equality
+            value.match = value.card1.Equals(value.card2) ? true : false;
+            // filename to read data from
+            string fileName = value.game + ".json";
+            // file location
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, configuration["GameDataDirectory"], fileName);
+
+            try
+            {
+                // read all contents from game datafile
+                var json = System.IO.File.ReadAllText(filePath);
+                var jsonObj = JObject.Parse(json);
+                // takeout rounds data for game
+                var rounds = jsonObj.GetValue("roundData") as JArray;
+                // add new round data into existing object
+                var newRoundData = JObject.Parse(JsonConvert.SerializeObject(new KeyValuePair<string, string>(value.card1, value.card2)));
+                rounds.Add(newRoundData);
+
+                jsonObj["roundData"] = rounds;
+                string updatedData = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj,
+                                       Newtonsoft.Json.Formatting.Indented);
+                // write updated data into file back
+                System.IO.File.WriteAllText(filePath, updatedData);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occured while adding round data: " + ex.Message.ToString());
+            }
+
+            // return equality of file
+            return JsonConvert.SerializeObject(value);
+        }
+    }
+
+
+    // these classes will help us in databinding
+    public class Schema
+    {
+        public string level { get; set; }
+        public List<KeyValuePair<string, string>> roundData { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+
+    public class Round
+    {
+        public Guid game;
+        public string card1;
+        public string card2;
+        public bool? match;
+    }
+}
--- MemoryGameServer/Startup.cs
+++ MemoryGameServer/Startup.cs
@@ -25,12 +25,19 @@ namespace MemoryGameServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors("MyPolicy");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
